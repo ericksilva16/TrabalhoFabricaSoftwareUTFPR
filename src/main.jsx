3d753aa1dd9dc50b4248b06f5cd8c564be26784e
Erick <erickvini1606@gmail.com>
2025-09-17 22:24:24 -0300

    Inicio do projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
-import './Header.css';
+import './header.css';
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
